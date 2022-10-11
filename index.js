d601a5f991ba773b6423ea28e1e834818762e90c
Morgan Tolman <morgan.tolman04@gmail.com>
2022-10-10 18:43:01 -0700

    Added TM questions.  Safe revert point.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const Employee = require(`./lib/Employee`); 
-const Engineer = require(`./lib/Engineer`)
+const Engineer = require(`./lib/Engineer`);
+const inquirer = require(`inquirer`);
+
+const questions = [
+    {type: `input`,
+    name: `TMName`,
+    message: `What is your team managers name?`},
+    {type: `input`,
+    name: `TMID`,
+    message: `What is your team managers employee ID?`},
+    {type: `input`,
+    name: `TMEmail`,
+    message: `What is your team managers email?`},
+    {type: `input`,
+    name: `TMoffice`,
+    message: `What is your team managers office number?`},
+];
